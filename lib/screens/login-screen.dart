0d50bb532cbb4055e827a38c4144d1c6e3ad91a7
Kryk <zhintak@gmail.com>
2021-09-13 16:10:12 -0500

    Clase 7

---
+++ lib/screens/login-screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('imgs/4909661.jpg'),
+              fit: BoxFit.fill
+              )
+          )
+        )
+      ],
+    );
+  }
+}
